8b25688ea682fd85b7bddd72a56ac94c2686ad98
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-05-31 17:41:11 +0200

    added the captain recognition in show and created seeds

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :user
-  has_many :matches
-  has_many :memberships
+  has_many :matches, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   validates :team_name, presence: true
   validates :country, presence: true
   validates :city, presence: true
