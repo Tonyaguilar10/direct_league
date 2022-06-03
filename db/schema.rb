4b6574b53c0aa771fa7d0fd9a757467a95c0c386
jose aguilar <jose.aguilar.miranda@gmail.com>
2022-06-03 16:17:04 +0200

    accept challenge

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_06_03_092656) do
-
+ActiveRecord::Schema.define(version: 2022_06_03_134012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
